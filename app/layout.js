65c8e6d156369cfc38013d3eeb7e1a984b551712
Gabriel Moore <gabrielfmoore@gmail.com>
2026-04-27 12:15:59 -0500

    bounds

--- app/layout.js
+++ app/layout.js
@@ -31,7 +31,7 @@ export default function RootLayout({ children }) {
           href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&family=Roboto:ital,wght@0,100..900;1,100..900&display=swap"
         />
       </head>
-      <body>
+      <body className="max-w-full">
         <ScrollToTopOnRouteChange />
         <Script
           src="https://kit.fontawesome.com/f92549a95d.js"
